cadcc929b0b8b8900dc18ce2f6d70b3d2d5d6f20
Lewis <93725160+LewisMagangi@users.noreply.github.com>
2024-09-17 13:56:57 +0300

    Time: 11 ms (83.42%), Space: 11.9 MB (37.38%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -5,9 +5,10 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        s_index, t_index = 0, 0
-        while s_index < len(s) and t_index < len(t):
-            if s[s_index] == t[t_index]:
-                s_index += 1
-            t_index += 1
-        return s_index == len(s)
+        for c in s:
+            i = t.find(c)
+            if i == -1:
+                return False
+            else:
+                t = t[i+1:]
+        return True
