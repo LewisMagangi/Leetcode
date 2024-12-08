4101a16d49b751a7abe39a172307543971c732eb
Lewis <93725160+LewisMagangi@users.noreply.github.com>
2024-12-08 18:28:25 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -26,10 +26,10 @@ class Solution(object):
                 else:
                     result.append([nums[i], nums[left], nums[right]])
 
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
 
                     left += 1
                     right -= 1
