09f5bc369e8c7820182e0285a37e0b8b3ec5784f
Lewis <93725160+LewisMagangi@users.noreply.github.com>
2025-02-24 11:17:13 +0300

    Time: 0 ms (100%), Space: 12.6 MB (13.35%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -5,7 +5,5 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        if len(p) != len(s.split()):
-            return False
-        return len(set(zip(p, s.split()))) == len(set(p)) == len(set(s.split()))
+        return len(p) == len(s.split()) and len(set(zip(p, s.split()))) == len(set(p)) == len(set(s.split()))
         
