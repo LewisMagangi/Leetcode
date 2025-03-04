45e9fa2674ed76096cdfe5c6f5bb413bed7d2536
Lewis <93725160+LewisMagangi@users.noreply.github.com>
2025-03-04 13:08:55 +0300

    Time: 5 ms (91.64%), Space: 17.4 MB (76.56%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -11,14 +11,31 @@ class Solution(object):
         :type postorder: List[int]
         :rtype: Optional[TreeNode]
         """
-        if not inorder:
-            return None
+        # Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def buildTree(self, inorder, postorder):
+        """
+        :type inorder: List[int]
+        :type postorder: List[int]
+        :rtype: Optional[TreeNode]
+        """
+        inorder_idx = { v:i for i, v in enumerate(inorder) }
 
-        root = TreeNode(postorder.pop())
-        index = inorder.index(root.val)
+        def helper(l, r):
+            if l > r:
+                return None
 
-        root.right = self.buildTree(inorder[index + 1:], postorder)
-        root.left = self.buildTree(inorder[:index], postorder)
+            root = TreeNode(postorder.pop())
+            index = inorder_idx[root.val]
 
-        return root
+            root.right = helper(index + 1, r)
+            root.left = helper(l, index - 1)
+
+            return root
+        return helper(0, len(inorder) - 1) 
         
