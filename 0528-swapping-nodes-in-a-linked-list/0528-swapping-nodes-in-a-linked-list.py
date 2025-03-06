eaa49dc1c7bb5661abded0ffa8aab6c6945e406d
Lewis <93725160+LewisMagangi@users.noreply.github.com>
2025-03-06 16:32:10 +0300

    Time: 48 ms (78.15%), Space: 86.5 MB (53.22%) - LeetHub

--- 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.py
+++ 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.py
@@ -10,20 +10,17 @@ class Solution(object):
         :type k: int
         :rtype: Optional[ListNode]
         """
-        curr = head
-        count = 0
-
-        while curr:
-            curr = curr.next
-            count += 1
-
         first = head
+
         for _ in range(k - 1):
             first = first.next
-
-        second = head        
-        for _ in range(count - k):
+        
+        second = head
+        curr = first
+        while curr.next:
+            curr = curr.next
             second = second.next
 
         first.val, second.val = second.val, first.val
+       
         return head
