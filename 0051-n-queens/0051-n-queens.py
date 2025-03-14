46f00f175f2df55d12e05326fa34d0d166b5e1ad
Lewis <93725160+LewisMagangi@users.noreply.github.com>
2025-03-14 19:10:05 +0300

    Time: 12 ms (73.39%), Space: 12.7 MB (48.76%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -4,50 +4,33 @@ class Solution(object):
         :type n: int
         :rtype: List[List[str]]
         """
-        state = []
         solutions = []
-        self.search(state, solutions, n)
+        diag1 = set()
+        diag2 = set()
+        cols = set()
+
+        def backtrack(row, state):
+            if row == n:
+                solutions.append(['.' * i + 'Q' + '.' * (n - i - 1) for i in state])
+                return
+
+            for col in range(n):
+                if col in cols or (row - col) in diag1 or (row + col) in diag2:
+                    continue
+            
+                state.append(col)
+                cols.add(col)
+                diag1.add(row - col)
+                diag2.add(row + col)
+
+                backtrack(row + 1, state)
+
+                state.pop()
+                cols.remove(col)
+                diag1.remove(row - col)
+                diag2.remove(row + col)
+
+        backtrack(0, [])
         return solutions
 
-    def get_candidates(self, state, n):
-        if not state:
-            return range(n)
-
-        position = len(state)
-        candidates = set(range(n))
-
-        for row, col in enumerate(state):
-            candidates.discard(col)
-            distance = position - row
-            candidates.discard(col + distance)
-            candidates.discard(col - distance)
-        return candidates
-
-    def search(self, state, solutions, n):
-        """
-        Checks if a state is valid and if it is it add the state to the solutions
-        """
-        if self.is_valid(state, n):
-            state_string = self.state_to_string(state, n)
-            solutions.append(state_string)
-            return 
-
-        for candidate in self.get_candidates(state, n):
-            state.append(candidate)
-            self.search(state, solutions, n)
-            state.pop()
-    
-    def is_valid(self, state, n):
-        """
-        Checks if the new proposed position is valid ?
-        """
-        return len(state) == n
-
-    def state_to_string(self, state, n):
-        res = []
-        for i in state:
-            string = '.' * i + 'Q' + '.' * (n - i - 1)
-            res.append(string)
-        return res
-        
         
