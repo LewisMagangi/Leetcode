4a3b78654ab3893351ee8815548a10ff65a43f98
Lewis <93725160+LewisMagangi@users.noreply.github.com>
2025-03-18 14:19:50 +0300

    Time: 0 ms (100%), Space: 12.3 MB (81.08%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
@@ -11,9 +11,8 @@ class Solution(object):
             n += 1
 
         while n > 0:
-            half = 1 << (n - 1)
-            if k > half:
-                k -= half
+            if k > 1 << (n - 1):
+                k -= 1 << (n - 1)
                 shift += 1
             n -= 1
         return chr(ord('a') + shift)
