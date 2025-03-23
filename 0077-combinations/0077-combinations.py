7a2a7c7a7f973b29106e3dc27cd553c77f317f9c
Lewis <93725160+LewisMagangi@users.noreply.github.com>
2025-03-23 12:43:43 +0300

    Time: 27 ms (97.44%), Space: 125.2 MB (97.95%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,3 +1,5 @@
+import itertools
+
 class Solution(object):
     def combine(self, n, k):
         """
@@ -5,16 +7,4 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
-        res = []
-        def backtrack(start, combination):
-            if len(combination) == k:
-                res.append(combination[:])
-                return
-            
-            for i in range(start, n + 1):
-                combination.append(i)
-                backtrack(i + 1, combination)
-                combination.pop()
-
-        backtrack(1, [])
-        return res
+        return list(itertools.combinations(range(1, n + 1), k))
