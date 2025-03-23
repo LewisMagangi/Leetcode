2516e7bb6f74fe74bd024078c947432613f1fb17
Lewis <93725160+LewisMagangi@users.noreply.github.com>
2025-03-23 19:29:25 +0300

    Time: 0 ms (100%), Space: 12.8 MB (15.28%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        stack = []
+        result = []
+
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                result.append(''.join(stack))
+                return
+            
+            if openN < n:
+                stack.append('(')
+                backtrack(openN + 1, closedN)
+                stack.pop()
+            
+            if closedN < openN:
+                stack.append(')')
+                backtrack(openN, closedN + 1)
+                stack.pop()
+        backtrack(0, 0)
+        return result
