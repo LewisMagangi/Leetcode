4aa0907e9bd1b4c16076e538f9357ff0e42b0682
Lewis <93725160+LewisMagangi@users.noreply.github.com>
2025-03-27 12:25:55 +0300

    Time: 10 ms (55.59%), Space: 13.4 MB (84.82%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Solution(object):
     def canFinish(self, numCourses, prerequisites):
         """
@@ -5,31 +7,24 @@ class Solution(object):
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
-        Prerequisite_map = { i:[] for i in range(numCourses)}
+        adjacency_list = { i:[] for i in range(numCourses)}
+
+        in_degree = [0] * numCourses
 
         for course, prerequisite in prerequisites:
-            Prerequisite_map[course].append(prerequisite)
+            adjacency_list[prerequisite].append(course)
+            in_degree[course] += 1
 
-        visited = set()
+        queue = deque([i for i in range(numCourses) if in_degree[i] == 0])
+        processed_courses = 0
 
-        def dfs(course):
-            if course in visited:
-                return False
-            if Prerequisite_map[course] == []:
-                return True
-            
-            visited.add(course)
+        while queue:
+            course = queue.popleft()
+            processed_courses += 1
 
-            for prerequisite in Prerequisite_map[course]:
-                if not dfs(prerequisite):
-                    return False
-            
-            visited.remove(course)
-            Prerequisite_map[course] = []
-            return True
-
-        for course in range(numCourses):
-            if not dfs(course):
-                return False
-        return True            
+            for dependent in adjacency_list[course]:
+                in_degree[dependent] -= 1
+                if in_degree[dependent] == 0:
+                    queue.append(dependent)
         
+        return processed_courses == numCourses
