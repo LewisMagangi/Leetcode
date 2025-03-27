c05be60d00296fd7943f671c84b7359edbd7233d
Lewis <93725160+LewisMagangi@users.noreply.github.com>
2025-03-27 15:35:53 +0300

    Time: 0 ms (100%), Space: 12.4 MB (55.79%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -4,27 +4,11 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        l, r = 0, len(matrix) - 1
+        # Step 1: Transpose the matrix (swap rows with columns)
+        for i in range(len(matrix)):
+            for j in range(i, len(matrix)):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-        while l < r:
-            for i in range(r - l):
-                top, bottom = l, r  # Define top and bottom boundaries
-
-                # Store the top-left value
-                topleft = matrix[top][l + i]
-
-                # Move bottom-left to top-left
-                matrix[top][l + i] = matrix[bottom - i][l]
-
-                # Move bottom-right to bottom-left
-                matrix[bottom - i][l] = matrix[bottom][r - i]
-
-                # Move top-right to bottom-right
-                matrix[bottom][r - i] = matrix[top + i][r]
-
-                # Move stored top-left to top-right
-                matrix[top + i][r] = topleft
-
-            # Move to the next inner matrix
-            r -= 1
-            l += 1
+        # Step 2: Reverse each row    
+        for row in matrix:
+            row.reverse()
