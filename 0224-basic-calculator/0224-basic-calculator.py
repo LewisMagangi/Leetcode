00595889fff8b8165a04a1a22f6d614d909448fc
Lewis <93725160+LewisMagangi@users.noreply.github.com>
2025-03-27 18:55:07 +0300

    Time: 55 ms (75.48%), Space: 13.2 MB (48.2%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -4,29 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        stack = []
-        result = 0
-        num = 0
-        sign = 1
+        num_stack = []  # Stack for previous results before '('
+        op_stack = []    # Stack for signs before '('
+        num = 0          # Current number
+        result = 0       # Running total result
+        sign = 1         # 1 for '+', -1 for '-'
 
         for char in s:
-            if char.isdigit():
+            if char.isdigit():  # Build number
                 num = num * 10 + int(char)
-            
-            elif char in ['+', '-']:
+            elif char in ["+", "-"]:  # Apply previous number and change sign
                 result += sign * num
                 num = 0
-                sign = 1 if char == '+' else -1
-            
-            elif char == '(':
-                stack.append(result)
-                stack.append(sign)
+                sign = 1 if char == "+" else -1
+            elif char == "(":  # Push current result & sign onto stack
+                num_stack.append(result)
+                op_stack.append(sign)
                 result = 0
                 sign = 1
-
-            elif char == ')':
-                result += sign * num
+            elif char == ")":  # Complete parentheses evaluation
+                result += sign * num  # Add last number before ')'
                 num = 0
-                result *= stack.pop()
-                result += stack.pop()
-        return result + (sign * num)
+                result *= op_stack.pop()  # Apply sign before '('
+                result += num_stack.pop()  # Add result before '('
+
+        return result + (sign * num)  # Add last number if any
