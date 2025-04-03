85c991d49dc56d5476b8566e57d70707cf867645
Lewis <93725160+LewisMagangi@users.noreply.github.com>
2025-04-03 14:33:24 +0300

    Time: 0 ms (100%), Space: 13.1 MB (77.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,13 +5,10 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        
         nums_to_index = {}
 
         for index, no in enumerate(nums):
+            complement = target -  no
+            if complement in nums_to_index:
+                return [nums_to_index[complement], index]
             nums_to_index[no] = index
-
-        for index, no in enumerate(nums):
-            complement = target - no
-            if complement in nums_to_index and nums_to_index[complement] != index:
-                return (index, nums_to_index[complement])
