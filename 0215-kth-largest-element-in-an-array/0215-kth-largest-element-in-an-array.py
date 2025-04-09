b4ebb493b0bff9d4ff4738adcf15c1115556af1d
Lewis <93725160+LewisMagangi@users.noreply.github.com>
2025-04-09 17:19:37 +0300

    Time: 256 ms (68.99%), Space: 19.5 MB (70.85%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -7,5 +7,11 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        return heapq.nlargest(k, nums)[-1]
+        min_heap = nums[:k]
+        heapq.heapify(min_heap)
+
+        for num in nums[k:]:
+            if num > min_heap[0]:
+                heapq.heappushpop(min_heap, num)
+        return min_heap[0]
         
