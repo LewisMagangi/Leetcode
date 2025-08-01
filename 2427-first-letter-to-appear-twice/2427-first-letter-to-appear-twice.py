22e143b07b153004a931f14f948e47729c1665fc
Lewis <93725160+LewisMagangi@users.noreply.github.com>
2025-08-01 10:24:14 +0300

    Time: 0 ms (100%), Space: 12.3 MB (77.43%) - LeetHub

--- 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.py
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.py
@@ -1,12 +1,18 @@
+from collections import defaultdict
+
 class Solution(object):
     def repeatedCharacter(self, s):
         """
         :type s: str
         :rtype: str
         """
-        l = []
-        for i in s:
-            if i in l:
-                return i
-            else:
-                l.append(i)
+        """
+        1. create a hashmap = {letter: no of appearance}
+        2. Iterate the first two appearances of a letter break and return the letter
+        """
+        letter_appearances = defaultdict(int)
+
+        for letter in s:
+            if letter in letter_appearances:
+                return letter
+            letter_appearances[letter] += 1
