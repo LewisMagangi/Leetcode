d1a278aa727e87d867420617e1bb3fa7a99ff613
Lewis <93725160+LewisMagangi@users.noreply.github.com>
2026-07-28 13:08:03 +0300

    Time: 0 ms (100%), Space: 12.5 MB (17.39%) - LeetHub

---
+++ 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def distMoney(self, money, children):
+        """
+        :type money: int
+        :type children: int
+        :rtype: int
+        """
+        money -= children
+
+        if money < 0:
+            return -1
+        
+        if (money // 7 == children and money % 7 == 0):
+            return children
+
+        if (money // 7 == children - 1 and money % 7 == 3):
+            return children - 2
+        
+        return min(children - 1, money // 7)
